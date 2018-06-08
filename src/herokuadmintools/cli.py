7592a011e354504b5f5cdc98e4293fc9c64cbe7f
Hal Wine <hwine@mozilla.com>
2018-06-08 11:53:50 -0700

    Support Magic Mail Merge
    I use the gSuite addon "Magic Mail Merge" to notify folks. This addition
    outputs csv in the format I need.

--- src/herokuadmintools/cli.py
+++ src/herokuadmintools/cli.py
@@ -8,6 +8,7 @@ Exit Codes:
     3 - Heroku query failure
 """
 import argparse
+from collections import defaultdict
 import logging
 
 from herokuadmintools import (
@@ -42,6 +43,21 @@ def output_results(users_missing_2fa, affected_apps, org=ORG_NAME_DEFAULT):
             print('{} ({})'.format(app, ', '.join(sorted(emails))))
 
 
+def generate_email_csv(users_missing_2fa, affected_apps, cc_addr=None):
+    # flip app: users to user: apps
+    user_apps = defaultdict(list)
+    for app, users in affected_apps.items():
+        for user in users:
+            user_apps[user].append(app)
+    if cc_addr:
+        print('"CC",', end='')
+    print('"{}","{}"'.format("Email Address", "Apps"))
+    for user, apps in user_apps.items():
+        if cc_addr:
+            print('"{}",'.format(cc_addr), end='')
+        print('"{}","{}"'.format(user, ','.join(apps)))
+
+
 def do_task(args):
     org = args.organization
     verify_access(org)
@@ -50,6 +66,8 @@ def do_task(args):
 
     if args.csv:
         generate_csv(users_missing_2fa, affected_apps)
+    elif args.email:
+        generate_email_csv(users_missing_2fa, affected_apps, args.cc)
     else:
         output_results(users_missing_2fa, affected_apps)
 
@@ -64,9 +82,18 @@ def parse_args(args=None):
                         action='store_true')
     parser.add_argument('--csv', action='store_true',
                         help='output as csv file')
+    parser.add_argument('--email', action='store_true',
+                        help='output as csv file, email order')
+    parser.add_argument('--cc', help='Specify CC column for --email output',
+                        default=None)
     parser.add_argument('--organization', help='Heroku Organization to query',
                         default=ORG_NAME_DEFAULT)
     args = parser.parse_args(args=args)
+    # handle args which imply others
+    if args.cc and not args.email:
+        args.email = True
+    if args.email and args.csv:
+        parser.error("Only one of --email and --csv can be specified")
     if args.debug:
         logger.setLevel(logging.DEBUG)
     return args
